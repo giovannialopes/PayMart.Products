9f9331fa27bb49b9c65512e8fe6b752187665789
Giovanni Lopes <giovannialopes.dev@gmail.com>
2024-08-19 19:50:53 -0300

    feature/ Implementação do banco de dados SQL Server.

--- Source/PayMart.Infrastructure.Products/Injection/DepedencyInjectionInfra.cs
+++ Source/PayMart.Infrastructure.Products/Injection/DepedencyInjectionInfra.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PayMart.Domain.Products.Interface.Database;
+using PayMart.Infrastructure.Products.DataAcess;
+using PayMart.Infrastructure.Products.Repositories;
 
 namespace PayMart.Infrastructure.Products.Injection;
 
 public static class DepedencyInjectionInfra
 {
-    private static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        AddDbContext(services, configuration);
+    }
+    
+    public static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<ICommit, ProductRepository>();
 
+        var connectionString = configuration.GetConnectionString("Connection");
+        services.AddDbContext<DbProduct>(config => config.UseSqlServer(connectionString));
     }
 }
---
+++ Source/PayMart.Infrastructure.Products/Repositories/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using PayMart.Domain.Products.Interface.Database;
+using PayMart.Infrastructure.Products.DataAcess;
+
+namespace PayMart.Infrastructure.Products.Repositories;
+
+public class ProductRepository :
+    ICommit
+{
+    private readonly DbProduct _dbProduct;
+
+    public ProductRepository(DbProduct dbProduct)
+    {
+        _dbProduct = dbProduct;
+    }
+
+    public Task Commit()
+    {
+        throw new NotImplementedException();
+    }
+}
