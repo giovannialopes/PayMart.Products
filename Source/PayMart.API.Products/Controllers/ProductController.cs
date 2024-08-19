81db102667908a1235d55ab431f64b57be165d45
Giovanni Lopes <giovannialopes.dev@gmail.com>
2024-08-19 19:29:56 -0300

    Feature/ Criação da classe Product em Domaind/Entities

---
+++ Source/PayMart.API.Products/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PayMart.API.Products.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    [HttpPost]
+    [Route("post")]
+    public async Task<IActionResult> Post(
+    [FromServices] HttpClient http)
+    {
+
+    }
+}
---
+++ Source/PayMart.Domain.Products/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace PayMart.Domain.Products.Entities;
+
+public class Product
+{
+    public int ID { get; set; }
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public int Amount { get; set; } 
+    public decimal Price { get; set; }
+    public int UserID {  get; set; }
+   
+}
