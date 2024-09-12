1b1f815118e4a407e30f33a053c8ce4f778f9ab0
Giovanni Lopes <giovannialopes.dev@gmail.com>
2024-09-11 23:45:24 -0300

    feat: criação do docker na aplicação.

--- src/PayMart.API.Products/Program.cs
+++ src/PayMart.API.Products/Program.cs
@@ -4,8 +4,13 @@ using PayMart.Domain.Products.Services.AInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// AddProduct services to the container.
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
+// AddProduct services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,6 +27,16 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+if (app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PayMart.Products API V1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
